0d10721be445d6e453a389881d2f2514656765f3
Xin Xu <xinxu0x@gmail.com>
2016-05-03 05:51:06 +0800

    layout match ui

---
+++ ios/admin/admin/Views/PlayerTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerTableViewCell.swift
+//  admin
+//
+//  Created by tassar on 5/3/16.
+//  Copyright © 2016 pulupulu. All rights reserved.
+//
+
+import Cocoa
+
+class PlayerTableViewCell: UITableViewCell {
+
+}
