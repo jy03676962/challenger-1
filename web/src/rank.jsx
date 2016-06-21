00042a6b14a1b43879cbbe41d7593457173c1d49
tassar <xinxu0x@gmail.com>
2016-06-21 23:45:39 +0800

    switch rank mode at regular intervals
    TODO:
    - we should pull data in advance
    - pulling data rate may be slower than switching mode rate
    - at the beginning, we should pull general mode data first, and then pull survival data in the following.
    - but anyway we need a loading indicator while pulling data

--- web/src/rank.jsx
+++ web/src/rank.jsx
@@ -1,24 +1,55 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { observable, computed } from 'mobx'
+import { observable } from 'mobx'
 import { observer } from 'mobx-react'
 import CSSModules from 'react-css-modules'
-import styles from '~/styles/rank.css'
-import * as util from '~/js/util.jsx'
 import Enum from 'es6-enum'
+
+import SetIntervalMixin from '~/js/mixins/SetIntervalMixin.jsx'
+import styles from '~/styles/rank.css'
 
 const RankMode = Enum('SurvivalMode', 'GeneralMode')
 const ChangeModeInterval = 10
 
 class Rank {
   @ observable survivalModeData
-  @ observable goldModeData
-  @ observable error
+  @ observable generalModeData
   @ observable mode
 }
 
+const RankDataView = CSSModules(React.createClass({
+  render() {
+    let rank = this.props.rank
+    let data = rank.mode == RankMode.SurvivalMode ? survivalModeData : generalModeData
+    if (data == null || data.error.length != 0) {
+      return null
+    }
+    return (
+      <div>
+      </div>
+    )
+  }
+
+}), styles)
+
 const RankView = CSSModules(observer(React.createClass({
-  render() {}
+  mixins: [SetIntervalMixin],
+  componentDidMount: () => {
+    this.setInterval(this.changeMode, ChangeModeInterval * 1000)
+  },
+  changeMode: () => {},
+  render() {
+    let rank = this.props.rank
+    let data = rank.mode == RankMode.SurvivalMode ? survivalModeData : generalModeData
+    return (
+      <div styleName='root'>
+        <div styleName='container'>
+          <img styleName='rootImg' src={require('./assets/qbg.png')} />
+          <RankDataView {...this.props} />
+        </div>
+      </div>
+    )
+  },
 })), styles)
 
 var rank = new Rank()
