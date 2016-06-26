2c05d8a2cff2cbdc7c5692193a011b36b605eef2
tassar <xinxu0x@gmail.com>
2016-06-26 21:41:10 +0800

    修复网页边框问题

--- server/public/js/api.js
+++ server/public/js/api.js
@@ -65,7 +65,7 @@
 /******/ 	}
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "da95628672812c9a63ba"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "18370480ccea274b426c"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
 /******/ 	
