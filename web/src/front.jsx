9edee645cccef97822a9a1780fa54e20e5f9aca5
tassar <xinxu0x@gmail.com>
2016-07-15 15:36:08 +0800

    更新取号页面UI

--- web/src/front.jsx
+++ web/src/front.jsx
@@ -7,60 +7,61 @@ import styles from '~/styles/front.css'
 
 
 class Front {
-  @observable number
+	@observable number
 }
 
 const FrontView = CSSModules(observer(React.createClass({
-  render() {
-    let number = this.props.front.number
-    return (
-      <div styleName='root'>
-        <input type='radio' name='num' ref='p1' value='p1' defaultChecked />1人
-        <input type='radio' name='num' ref='p2' value='p2' />2人
-        <input type='radio' name='num' ref='p3' value='p3'/>3人
-        <input type='radio' name='num' ref='p4' value='p4' />4人<br/><br/><br/>
-        <input type='radio' name='mode' ref='gold' value='gold' defaultChecked />赏金<br/>
-        <input type='radio' name='mode' ref='survival' value='survival' />生存<br/>
-        <button onClick={this.addTeam}>取号</button><br/><br/>
-        <button onClick={this.resetQueue}>重置</button><br/><br/>
-        <label>当前号码</label><br/>
-        {
-          number ? <label ref='currentNumber'>{number}</label> : null
-        }
-      </div>
-    )
-  },
-  addTeam: function(e) {
-    let front = this.props.front
-    var c = 1
-    if (this.refs.p1.checked) {
-      c = 1
-    } else if (this.refs.p2.checked) {
-      c = 2
-    } else if (this.refs.p3.checked) {
-      c = 3
-    } else {
-      c = 4
-    }
-    let param = {
-      count: c,
-      mode: this.refs.gold.checked ? 'g' : 's'
-    }
-    $.post('/api/addteam', param, function(data) {
-      if (data) {
-        front.number = data.id
-      }
-    })
-  },
-  resetQueue: function(e) {
-    var r = window.confirm('确定要重置吗？')
-    if (r == true) {
-      let front = this.props.front
-      $.post('/api/resetqueue', function(data) {
-        front.number = null
-      })
-    }
-  }
+	render() {
+		let number = this.props.front.number
+		return (
+			<div styleName='root'>
+				<div styleName='title'>暴走的金币</div>
+				<input type='radio' name='num' ref='p1' value='p1' defaultChecked /><span>1人</span>
+				<input type='radio' name='num' ref='p2' value='p2' /><span>2人</span>
+				<input type='radio' name='num' ref='p3' value='p3' /><span>3人</span>
+				<input type='radio' name='num' ref='p4' value='p4' /><span>4人</span><br/><br/><br/>
+				<input type='radio' name='mode' ref='gold' value='gold' defaultChecked /><span>赏金</span>
+				<input type='radio' name='mode' ref='survival' value='survival' /><span>生存</span><br/>
+				<button styleName='add' onClick={this.addTeam}>取号</button>
+				<label>当前号码</label><br/>
+				{
+					number ? <label ref='currentNumber'>{number}</label> : null
+				}
+				<button styleName='reset' onClick={this.resetQueue}>重置</button>
+			</div>
+		)
+	},
+	addTeam: function(e) {
+		let front = this.props.front
+		var c = 1
+		if (this.refs.p1.checked) {
+			c = 1
+		} else if (this.refs.p2.checked) {
+			c = 2
+		} else if (this.refs.p3.checked) {
+			c = 3
+		} else {
+			c = 4
+		}
+		let param = {
+			count: c,
+			mode: this.refs.gold.checked ? 'g' : 's'
+		}
+		$.post('/api/addteam', param, function(data) {
+			if (data) {
+				front.number = data.id
+			}
+		})
+	},
+	resetQueue: function(e) {
+		var r = window.confirm('确定要重置吗？')
+		if (r == true) {
+			let front = this.props.front
+			$.post('/api/resetqueue', function(data) {
+				front.number = null
+			})
+		}
+	}
 })), styles)
 
 var z = require('npm-zepto')
